1a7454d9cb88cb4bf0029333bf7de77fcc28519a
Gabriel <gabriel.le.roy06@gmail.com>
2024-03-15 18:06:37 +0100

    added a form contact

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const axios = require("axios");
-
 let connectButton = document.querySelector("header .bouton-connexion");
 let contactForm = document.getElementById("form-modal");
+let body = document.getElementById("body");
 
 connectButton.addEventListener("click", () => {
   contactForm.classList.remove("hidden");
+  body.classList.add("noscroll");
 });
 
 contactForm.addEventListener("submit", async (event) => {
-  event.preventDefault;
+  event.preventDefault();
   contactForm.classList.add("hidden");
+  body.classList.remove("noscroll");
 
   const data = {
     firstname: document.querySelector("#firstname").value,
@@ -18,6 +19,6 @@ contactForm.addEventListener("submit", async (event) => {
     subject: document.querySelector("#subject").value,
     message: document.querySelector("#message").value,
   };
-
+  console.log(data);
   const response = await axios.post("http://localhost:3000/form", data);
 });
